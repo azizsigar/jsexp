3f644f8a20b87bf5ef13521f72a5e2f506bb1be1
azizsigar <aazizsigar@gmail.com>
2023-09-21 13:33:02 +0200

    backtick

---
+++ backtick.js
@@ -0,0 +1,8 @@
+let catName = 'Minnie';
+let catAge = 5;
+
+let sentence = 'My cat is called ' + catName + ' , and is ' + catAge + ' years old.';
+
+// console.log(typeof `$D{sentence}`);
+// console.log (typeof("hello its a same thing"+ sentence))
+console.log(`its easy and modern way to write${sentence}`)
