5814be5caa8ef1e58a8619c29bd04bb8c9f4b348
azizsigar <aazizsigar@gmail.com>
2023-09-21 10:30:46 +0200

    obi

---
+++ obi.js
@@ -0,0 +1,6 @@
+const walksLikeADuck = true;
+const talksLikeADuck = false;
+
+let isDuck = (walksLikeADuck !== talksLikeADuck) ? "It's a duck" : "It's not a duck.";
+// i added !!!!! sign its checking opposite of the value
+console.log(isDuck);
