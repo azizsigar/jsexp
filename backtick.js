0b9a01a7a15d62a76c7403242342dbf7365d4965
azizsigar <aazizsigar@gmail.com>
2023-09-21 13:37:33 +0200

    v1

--- backtick.js
+++ backtick.js
@@ -5,4 +5,11 @@ let sentence = 'My cat is called ' + catName + ' , and is ' + catAge + ' years o
 
 // console.log(typeof `$D{sentence}`);
 // console.log (typeof("hello its a same thing"+ sentence))
-console.log(`its easy and modern way to write${sentence}`)
+// console.log(`its easy and modern way to write${sentence}`)
+
+
+
+// example 2
+let hello = "hello how are you"
+let name = "obi"
+console.log(`i want to say ${hello} ${name}`)
